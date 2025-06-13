db7f3e04e9f49c9cc8e5b8b83bca2b765c187c7b
killmur <32865668+killmur@users.noreply.github.com>
2025-06-13 23:11:24 +0100

    chore(release): Release config updated
    - Updated release.config.js to update versioning on multiple different release types.
    - Added noteKeywords of 'BREAKING CHANGE' to realease.config.js

--- release.config.js
+++ release.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      releaseRules: [
+        { type: 'feat', release: 'minor' },
+        { type: 'fix', release: 'patch' },
+        { type: 'chore', release: 'patch' },
+        { type: 'docs', release: 'patch' },
+        { type: 'refactor', release: 'patch' },
+        { type: 'style', release: 'patch' },
+        { type: 'test', release: 'patch' },
+        { type: 'build', release: 'patch' },
+        { type: 'ci', release: 'patch' }
+      ],
+      parserOpts: {
+        noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+      }
+    }],
     '@semantic-release/release-notes-generator',
     ['@semantic-release/changelog', {
       changelogFile: 'CHANGELOG.md',
